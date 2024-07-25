5b8adcc330c37c68faa56e8d38c9427756868829
Daniel Brumley <dbrumley90@gmail.com>
2024-07-25 10:00:09 -0400

    Complete exercises 01-07

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
+// const reverseString = function(string) {
+    
+//     let reversedString = "";
+//     for(let i=string.length-1; i>=0; i--) {
+//         reversedString += string[i];
+//     }
 
-};
+//     return reversedString;
+// };
+
+const reverseString = function (string) {
+    return string.split("").reverse().join("");
+  };
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        arr = arr.filter((x) => x !== arg)
+    }
+
+    return arr;
 };
 
 // Do not edit below this line
